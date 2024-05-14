df56f40150372da7d5db2d4437ace38603ccfb5a
Chandrakant Kushwah <chandrakantkushwah@Chandrakants-MacBook-Air.local>
2024-05-14 18:21:47 +0530

    added the cors and dotenv

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const leadsRoutes = require("./routes/leads");
 const authMiddleware = require("./middleware");
 
+// Load environment variables from .env file
+dotenv.config();
+
 // Initialize Express
 const app = express();
+
+// Apply CORS middleware
+app.use(cors());
 
 // parsing data
 app.use(express.json());
@@ -14,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Your routes and middleware will go here
 app.use("/auth", authRoutes);
-app.use("/leads", authMiddleware,leadsRoutes);
+app.use("/leads", authMiddleware, leadsRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
